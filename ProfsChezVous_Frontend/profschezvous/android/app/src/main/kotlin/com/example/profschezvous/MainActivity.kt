f9d3b9bab7160f08df5527cfac39108c5fc4bce5
Mohamedsalem00 <126581399+Mohamedsalem00@users.noreply.github.com>
2024-04-06 21:13:49 +0000

    réinitialiser toutes les tables

--- ProfsChezVous_Frontend/profschezvous/android/app/src/main/kotlin/com/example/profschezvous/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.profschezvous
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ profschezvousfrontend/android/app/src/main/kotlin/com/example/profschezvousfrontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.profschezvousfrontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
