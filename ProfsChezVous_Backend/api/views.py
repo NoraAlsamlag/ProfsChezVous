eca1a5c75e4df61582ca0716b7b606475e9c53e5
Boubacar <djigob616@gmail.com>
2024-04-18 16:52:00 +0100

    view comment

--- ProfsChezVous_Backend/api/views.py
+++ ProfsChezVous_Backend/api/views.py
@@ -2,24 +2,27 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import  *
 from .models import *
+from .models import *
+from .models import *
+from .models import *
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        {
-            'Endpoint':'/api/get_all', 
-            'Method':"GET",
-            'body':None,
-            'Description':'Returns all available data in the database'
-        },
-        {
-            "Endpoint":"/api/add_data",
-            "Method": "POST",
-            "Body":{
-                "Title":"String, required",
-                "Content":"String, required"
-            },
-            "Description":"Add a new blog post to the database."
-        }
-    ]
-    return Response(routes)
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         {
+#             'Endpoint':'/api/get_all', 
+#             'Method':"GET",
+#             'body':None,
+#             'Description':'Returns all available data in the database'
+#         },
+#         {
+#             "Endpoint":"/api/add_data",
+#             "Method": "POST",
+#             "Body":{
+#                 "Title":"String, required",
+#                 "Content":"String, required"
+#             },
+#             "Description":"Add a new blog post to the database."
+#         }
+#     ]
+#     return Response(routes)
