486cd0354e7a81a4a35601fab9dc146c580e8331
Mohamedsalem00 <126581399+Mohamedsalem00@users.noreply.github.com>
2024-04-18 13:31:19 +0000

    view modifier

--- ProfsChezVous_Backend/api/views.py
+++ ProfsChezVous_Backend/api/views.py
@@ -3,23 +3,23 @@ from rest_framework.response import Response
 from .serializers import  *
 from .models import *
 
-# @api_view(['GET'])
-# def getRoutes(request):
-#     routes = [
-#         {
-#             'Endpoint':'/api/get_all', 
-#             'Method':"GET",
-#             'body':None,
-#             'Description':'Returns all available data in the database'
-#         },
-#         {
-#             "Endpoint":"/api/add_data",
-#             "Method": "POST",
-#             "Body":{
-#                 "Title":"String, required",
-#                 "Content":"String, required"
-#             },
-#             "Description":"Add a new blog post to the database."
-#         }
-#     ]
-#     return Response(routes)
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint':'/api/get_all', 
+            'Method':"GET",
+            'body':None,
+            'Description':'Returns all available data in the database'
+        },
+        {
+            "Endpoint":"/api/add_data",
+            "Method": "POST",
+            "Body":{
+                "Title":"String, required",
+                "Content":"String, required"
+            },
+            "Description":"Add a new blog post to the database."
+        }
+    ]
+    return Response(routes)
